f678fe04b63875d89afc77be88dbd9bebe466c2c
FunaCat <75957785+FunaCat@users.noreply.github.com>
2024-03-03 09:10:02 -0500

    some random firebase

--- backend/mysite/manage.py
+++ backend/mysite/manage.py
@@ -2,7 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+import firebase_admin
+from firebase_admin import credentials
 
 def main():
     """Run administrative tasks."""
@@ -16,6 +17,11 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
+    cred = credentials.Certificate("\task-app-b58f4-firebase-adminsdk-2pkh6-39b7c7a042.json")
+    default_app = firebase_admin.initialize_app(cred, {
+        'databaseURL': databaseURL,
+    })
 
 
 if __name__ == '__main__':
