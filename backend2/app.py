8899e7088aa07542868a7bbc98c082335cec9283
Vivek M <66559749+viv511@users.noreply.github.com>
2024-03-03 13:44:24 -0500

    tried to do email

--- backend2/app.py
+++ backend2/app.py
@@ -4,6 +4,7 @@ from firebase import firebase
 import firebase_admin
 from firebase_admin import firestore, credentials, db
 
+import smtplib, ssl
 
 
 # Use a service account.
@@ -27,3 +28,41 @@ def index():
 @app.route('/verify')
 def verify():
     return "bo"
+
+
+
+
+
+def remindEmail(taskID, userID, emailAccount):
+    db = firestore.client()
+    users_ref = db.collection("users").document(userID)
+
+    name = users_ref.get().to_dict()["name"]
+
+    tasks_ref = users_ref.collection("tasks").document(taskID)
+    tasks_docs = tasks_ref.get()
+
+    tasks = [doc.to_dict() for doc in tasks_docs]
+    
+    title = tasks[0]["taskName"]
+    deadline = tasks[0]["deadlineTime"]
+
+    #Email time woo
+    port = 587
+    smtp_server = "smtp.gmail.com"
+    computadora = "itzael.markavion@dealoaks.com"
+    reciever = "yhmrt7@gmail.com" #emailAccount here!!
+    password = "yamfd6C$"
+    message = """\
+    Subject: Reminder to complete your task!
+
+    {name}, you have a task to complete! You need to {title} before {deadline}. \n Good luck!""".format(name=name, title=title, deadlineTime=tasks[0]["deadlineTime"])
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP(smtp_server, port) as server:
+        server.ehlo() # Can be omitted
+        server.starttls(context=context)
+        server.ehlo() # Can be omitted
+        server.login(computadora, password)
+        server.sendmail(computadora, reciever, message)
+
