590fba8349a025c497cdfb862b199641d2c723fc
FunaCat <75957785+FunaCat@users.noreply.github.com>
2024-03-03 13:06:28 -0500

    idk

--- backend2/app.py
+++ backend2/app.py
@@ -13,11 +13,10 @@ firebase_admin.initialize_app(cred)
 
 @app.route('/')
 def hello():    
-    ref = db.reference('/users/Ud8a2ZqK6Ucn9BDm2xuj/tasks/yP2sOyfpsXJvcu9lG4zj')
-    # db = firestore.client()
-    # users_ref = db.collection("users")
-    # docs = users_ref.get()
-    wow = ref.get()
     
-    # return [doc.to_dict() for doc in docs]
-    return wow
+    db = firestore.client()
+    users_ref = db.collection("users")
+    docs = db.collection("users").get().val()
+
+    #return [doc.to_dict() for doc in docs]
+    return docs
